c8b971904f5f2166323ac95929f135f4cdd2b821
gitblairshub <blairsarlo@gmail.com>
2022-02-03 18:46:53 -0500

    Add UI for IncomeExpenses component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import Balance from './components/Balance';
+import  Balance  from './components/Balance';
 import { Header } from './components/Header';
+import  IncomeExpenses  from './components/IncomeExpenses';
 
 function App() {
   return (
     <>
       <Header />
-      <Balance />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+      </div>
+      
     </>
   );
 }
---
+++ src/components/IncomeExpenses.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const IncomeExpenses = () => {
+  return (
+    
+    <div>
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className="money plus">+$0.00</p>
+      </div>
+      <div>
+        <h4>Income</h4>
+        <p id="money-plus" className="money plus">+$0.00</p>
+      </div>
+    </div>
+  )
+};
+
+export default IncomeExpenses;
