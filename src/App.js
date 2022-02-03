ca5a813ff5a9fdbe50d717c09c6c49489e9e12ee
gitblairshub <blairsarlo@gmail.com>
2022-02-03 18:12:13 -0500

    Add UI for Balance component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Balance from './components/Balance';
 import { Header } from './components/Header';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Balance />
+    </>
   );
 }
 
